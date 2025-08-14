31b41907c0a62ada6841732ba343ff4f1cfc5690
coparentsap-cyber <coparentsap@gmail.com>
2025-08-15 01:28:45 +0200

    Update vite.config.ts

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,2 +1,10 @@
-resolve: { alias: { '@': require('path').resolve(__dirname, 'src') } }
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  build: { outDir: 'dist' },
+  resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
+})
 
