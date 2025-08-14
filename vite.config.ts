f2a7b930a2af411451bf06811ca9ddcb54b76d8d
coparentsap-cyber <coparentsap@gmail.com>
2025-08-14 23:01:21 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000
-  }
+  build: { outDir: 'dist' }
 })
