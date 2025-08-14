fa6f8b92f6d05f5c9f1bb7fd1ac8cbfa367bef3a
coparentsap-cyber <coparentsap@gmail.com>
2025-08-14 20:42:44 +0200

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Bienvenue sur Co-Parents App</h1>
+      <p>Votre application pour gérer la garde de vos enfants sereinement.</p>
+    </div>
+  )
+}
+
+export default App
