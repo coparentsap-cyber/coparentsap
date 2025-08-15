02b27dff564e59084bfe520c5e464d23311fe63a
coparentsap-cyber <coparentsap@gmail.com>
2025-08-15 12:42:11 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: 'dist' },
-  resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
+  base: '/', // <-- important pour Netlify
 })
-
