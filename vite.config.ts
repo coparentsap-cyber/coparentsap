7b354395508cc2e6284de6e87c8f54b8b1abebc1
coparentsap-cyber <coparentsap@gmail.com>
2025-08-15 12:58:30 +0200

    fix: set base path for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // important pour que les assets JS/CSS se chargent sur Netlify
+  base: './', // important pour que les chemins JS/CSS fonctionnent sur Netlify
 })
